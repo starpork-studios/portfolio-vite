1678a53133c2231146438e6b5759102ae393966a
Jarrod van den Heuvel <jarrod.vandenheuvel@roamdigital.com>
2023-02-28 15:03:47 +1300

    added react-three-fiber and threejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -27,8 +28,17 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <mesh rotation={[10, 15, 6]}>
+          <boxGeometry args={[2, 2, 2]} />
+
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+      </Canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
