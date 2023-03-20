f5db6147187d2fb4ed0c8e94bcdf83a16641d48f
Jarrod van den Heuvel <jarrod.vandenheuvel@roamdigital.com>
2023-03-20 17:04:10 +1300

    refactor carousel logic

--- src/components/Home/CarouselVertical/CarouselVertical.tsx
+++ src/components/Home/CarouselVertical/CarouselVertical.tsx
@@ -8,8 +8,11 @@ import { CarouselProps } from "./types";
 export function CarouselVertical(props: CarouselProps) {
   const segmentRotate = (order: number, initial: number) => {
     if (order === 0) return initial;
+    return initial - order * 0.8;
+  };
 
-    return initial + ((2 * Math.PI) / 6.5) * (order * 0.8);
+  const scrollProportionFormula = (imageCount: number) => {
+    return (imageCount - 1) / 3;
   };
   // This reference will give us direct access to the mesh
   const group = useRef<Group>(null!);
@@ -37,27 +40,24 @@ export function CarouselVertical(props: CarouselProps) {
     const pastFirstHalfProgress = data.range(0, 1 / 2);
 
     // need to adjust this adding more photos
-    group.current.rotation.x = (data.offset / 3) * segmentRotate(3, 0);
+    // 5 photos = -data.offset * 4/3
+    // 4 photos = -data.offset
+    // 3 photos = -data.offset * 2/3
+    // 2 photos = -data.offset * 1/3
+    group.current.rotation.x =
+      -(data.offset * scrollProportionFormula(props.cyclinderData.length)) *
+      segmentRotate(3, 0);
   });
 
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <group {...props} ref={group} onClick={(event) => setActive(!active)}>
-      {props.cyclinderData
-        .slice(0)
-        .reverse()
-        .map((data, index) => (
-          <CylinderImage
-            rotation={[
-              segmentRotate(index, Math.PI / 1.9),
-              Math.PI / 7,
-              Math.PI,
-            ]}
-            file={data.file}
-            onSetHover={data.onSetHover}
-            hover={data.hover}
-          />
-        ))}
+      {props.cyclinderData.map((data, index) => (
+        <CylinderImage
+          rotation={[segmentRotate(index, 0), Math.PI / 7, Math.PI]}
+          file={data.file}
+        />
+      ))}
     </group>
   );
 }
