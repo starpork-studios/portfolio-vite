b9732b9ac1550d4ef4413fef74fd368994489e7d
jarrod-vdh <jarrodvdheuvel@gmail.com>
2023-11-03 21:25:38 +1300

    add fergus info

--- src/Modules/Home/Work/MyExperiences/Fergus.tsx
+++ src/Modules/Home/Work/MyExperiences/Fergus.tsx
@@ -1,26 +1,44 @@
 export const Fergus = {
   file: "fergus.png",
-  mobile: "lmi7mobile.jpeg",
+  mobile: "fergus-mobile.png",
   title: "Fergus",
   subtitle: "Fergus Go",
   page: {
     heading: "Fergus",
     year: "2023-24",
-    image: "Content/LMI-UI.png",
+    image: "Content/FERGUS-UI.png",
     sections: [
       {
         heading: "Background",
         paragraphs: [
-          <p>
-            Les Mills International has grown from a small family-owned gym to a
-            globally recognised brand with a presence in countries all around
-            the world.
-          </p>,
-          <p>
-            Les Mills hoped to boost acquisition, retention and referral numbers
-            by creating Les Mills Connect, a one-stop digital platform to
-            support Les Mills clubs and instructors around the world.
-          </p>,
+          <>
+            <p>
+              Fergus is the brainchild of Dan Pollard, a veteran Tradie. He
+              dreamed it up a few years ago, while he was knee deep in the
+              trenches of running his own trades business. Dan figured out that
+              a trades business needs proper systems in place to keep it running
+              without running its owner into the ground.
+            </p>
+            <br />
+            <p>
+              Fergus is the operational backbone of trades businesses, taking
+              care of the day-to-day and providing complete clarity and control.
+              It gives business owners and managers the time and insight they
+              need for the business to grow and succeed.
+            </p>
+          </>,
+          <>
+            <p>
+              Over time the Fergus mobile app has aged visually and its
+              framework has become dated, making it difficult for new developers
+              to work on and with less ecosystem support.
+            </p>
+            <br />
+            <p>
+              And so... the development of Fergus Go began, a new multi platform
+              mobile app built with the react native framework.
+            </p>
+          </>,
         ],
       },
       {
@@ -28,27 +46,30 @@ export const Fergus = {
         paragraphs: [
           <>
             <p>
-              I worked as a Frontend Engineer on Les Mills Connect forming a
-              strong working relationship with our client's multidisciplinary
-              team.
+              As a Software Engineer within the Fergus Go team, made up of
+              designers, product managers, QA's and other software engineers,
+              I've formed a great relationship with them and we've delivered
+              some awesome features and improvements to the platform.
             </p>
             <p>
-              I worked alongside designers, product managers, quality assurance
-              and other software engineers to collaboratively create and improve
-              Connect to be a product that we're all proud of.
+              I've been responsible for leading feature developments - breaking
+              down tasks, creating concise tickets for the team, facilitating
+              discussions on feature architecture, developing solutions,
+              reviewing code and coordinating with the team to mitigate
+              blockers.
             </p>
           </>,
           <>
             <p className=" mb-4">
-              While working on this project, I had a range of technical
+              While working on this project, I've had a range of technical
               responsibilities
             </p>
             <ul className="list-disc list-inside">
-              <li>Building UI components</li>
-              <li>Creating & maintaining full end to end automation tests</li>
-              <li>Integrating analytics systems</li>
-              <li>Developing responsiveness and accessibility</li>
-              <li>Implementing CI/CD</li>
+              <li>Building React Native UI components</li>
+              <li>Creating & maintaining tests</li>
+              <li>Building GraphQL API's</li>
+              <li>Offline management and caching</li>
+              <li>Performance and error logging management with Sentry</li>
             </ul>
           </>,
         ],
@@ -58,16 +79,16 @@ export const Fergus = {
         paragraphs: [
           <div>
             <p>
-              Les Mills Connect is now a web app used by hundreds of thousands
-              of users worldwide.
+              Fergus Go has been growing in popularity as its feature set has
+              expanded
             </p>
-            <p className="text-[56px] h-auto">100k+</p>
-            <p>Active users</p>
+            <p className="text-[56px] h-auto">5000+</p>
+            <p>Daily users</p>
           </div>,
           <div>
-            <p>Connect is being used all over the world</p>
-            <p className="text-[56px] h-auto">30+</p>
-            <p>Countries</p>
+            <p>App stability</p>
+            <p className="text-[56px] h-auto">90%</p>
+            <p>Error reduction</p>
           </div>,
         ],
       },
